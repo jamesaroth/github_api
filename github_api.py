c5165a5d497d048744de52e0904a5535babc4d75
jamesaroth <james.a.roth@gmail.com>
2019-03-03 14:42:56 -0500

    fix initial upload which included obsolete token

--- github_api.py
+++ github_api.py
@@ -9,34 +9,17 @@ username = f.readlines(1)
 username = username[0].strip()
 pwd = f.readlines(2)
 pwd = pwd[0].strip()
-headers = {"Authorization": "token" + token}
+headers = {"Authorization": "token " + token}
 
 repo_list = requests.get(base + username + "/repos", auth=(username, token))
 for repo in repo_list.json():
-    n = repo.get('name')
-    des = repo.get('description')
-    own = repo.get("owner")
+    print(repo.get('name'))
 
 def create_repo(name):
     payload = {'name': name, 'description': 'created via API'}
-    c = requests.post(base_user + "repos", auth=(username, token), data=json.dumps(payload))
-    return json.loads(c.content)
-
-# create_repo("hihihi")
-
-# DELETE /repos/:owner/:repo
-# login = requests.delete('https://api.github.com/' + 'repos/' + user + '/' + repo, headers=headers)
+    c = requests.post(base_user + "repos", headers=headers, data=json.dumps(payload))
+    return c.json()
 
 def delete_repo(name):
     d = requests.delete(root + "repos/" + username + "/" + name, auth=(username, token))
     return d
-
-
-
-# for repo in repo_list.json():
-#     print(repo.get("name"))
-
-
-
-# curl -H "Authorization: token e581df6fbee1a0a54e43a1af267578c80db80cda" https://api.github.com
-
